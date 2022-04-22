e8bd62ac52aa87d022db28627c25849dea8e215d
NanezX <victor.nanezj@gmail.com>
2022-04-22 15:03:22 -0400

    deploy script

---
+++ scripts/deploy.ts
@@ -0,0 +1,27 @@
+import hre, { ethers } from "hardhat";
+import * as path from "path";
+import { saveGameAssetsFactory } from "./utils";
+
+async function main() {
+  const GameAssetsFactoryFactory = await hre.ethers.getContractFactory(
+    "GameAssetsFactory"
+  );
+  const gameAssetsFactory = await GameAssetsFactoryFactory.deploy();
+
+  await gameAssetsFactory.deployed();
+
+  saveGameAssetsFactory(gameAssetsFactory);
+
+  console.log("GameAssetsFactory deployed ");
+}
+
+main()
+  .then(() => {
+    const exit = process.exit;
+    exit(0);
+  })
+  .catch((error) => {
+    console.error(error);
+    const exit = process.exit;
+    exit(1);
+  });
---
+++ scripts/utils.ts
@@ -0,0 +1,33 @@
+import hre from "hardhat";
+import * as path from "path";
+import fs from "fs";
+
+import type { Contract } from "ethers";
+
+/**
+ * Write a file
+ * @param _path Location of the file
+ * @param file The file
+ */
+export function writeFile(_path: string, file: any): void {
+  try {
+    fs.writeFileSync(_path, file);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+export function saveGameAssetsFactory(contract: Contract): void {
+  const config = {
+    network: hre.network.name,
+    gameAssetsFactory: contract.address,
+    gameAssetsFactoryBlock: contract.deployTransaction.blockNumber,
+  };
+
+  const pathConfigLocal = path.resolve(
+    __dirname,
+    `../config/${hre.network.name}.json`
+  );
+  writeFile(pathConfigLocal, JSON.stringify(config, null, 2));
+}
